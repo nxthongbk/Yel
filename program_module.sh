62bdd8c06e0703787e7e859f6ccdb10d2d5b141b
Thong Nguyen <nxthongbk@gmail.com>
2019-06-24 17:27:40 +0700

    update

--- program_module.sh
+++ program_module.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp test_script_mangOH_red.py root@192.168.2.2:~/test_script_mangOHYellow.py
+scp test_script_mangOHYellow.py root@192.168.2.2:~/test_script_mangOHYellow.py
 rc=$?; if [[ $? -ne 0 ]]; then exit $rc; fi
