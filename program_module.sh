c3f6f888adbf776d3acf90de7eb9a990bf8487bc
Thong Nguyen <nxthongbk@gmail.com>
2019-06-26 22:04:23 +0700

    update spk

--- program_module.sh
+++ program_module.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+set -x
+
+swiflash -m wp85 -r
+rc=$?; if [[ $? -ne 0 ]]; then exit $rc; fi
+
+sleep 60
+
+swiflash -m wp85 -i le19010_wp85_mangoh_yellow.spk
+rc=$?; if [[ $? -ne 0 ]]; then exit $rc; fi
+
+sleep 90
+
 scp test_script_mangOH_yellow.py root@192.168.2.2:~/test_script_mangOH_yellow.py
 rc=$?; if [[ $? -ne 0 ]]; then exit $rc; fi
